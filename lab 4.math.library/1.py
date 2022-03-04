2657b98f160dad4e6b5e89c21be238bdfeeae8e5
Arsen <arsennusip@gmail.com>
2022-03-04 17:25:54 +0600

    added lab4.math.library

---
+++ lab 4.math.library/1.py
@@ -0,0 +1,8 @@
+import math
+from math import*
+def get_radian():
+    k = int(input())
+    print('Input degree:',  k)
+    rad = (k * round(pi, 3))/180
+    print("Output radian:", round(rad, 6))
+get_radian()
---
+++ lab 4.math.library/3.py
@@ -0,0 +1,9 @@
+import math
+from math import*
+def find_regular_polygon(n, a):
+    print('Input number of sides:', n)
+    print('Input the length of a side:', a)
+    S = int(n*(math.pow(a, 2))/(4*floor(tan(180/n))))
+    print('Expected Output:', abs(S))
+x, y = map(int, input().split())
+find_regular_polygon(x, y)
